3eea2a119eb1ab6345d9a774e81db29ce4738054
Zsetta <134535531+Zsetta@users.noreply.github.com>
2023-12-18 02:24:05 -0300

    1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  const tuvieja = 1
   return (
     <div>
       <Header course={course}/>
